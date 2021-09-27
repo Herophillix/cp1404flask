a2020a7a24fc6479121fbd123f1513a6f841aeb5
James Makarios <jamesmakarioslo@gmail.com>
2021-09-27 22:52:54 +0800

    Add Practical 10

--- app.py
+++ app.py
@@ -13,9 +13,23 @@ def hello_world():  # put application's code here
 def greet(name=""):
     return "Hello {0}".format(name)
 
+
 @app.route('/f/<degree>')
-def greet(degree=0):
-    return "Hello {0}".format(degree)
+def to_fahrenheit(degree=""):
+    try:
+        return_value = "{0}".format(float(degree) * 9.0 / 5 + 32)
+    except ValueError:
+        return_value = ""
+    return return_value
+
+
+@app.route('/c/<degree>')
+def to_celsius(degree=""):
+    try:
+        return_value = "{0}".format(5 / 9 * (float(degree) - 32))
+    except ValueError:
+        return_value = ""
+    return return_value
 
 
 if __name__ == '__main__':
